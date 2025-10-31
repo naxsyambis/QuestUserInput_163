f826b92658a5a34441a7322bdf4f9d44d169aa1f
naxsyambis <naxsyambis@gmail.com>
2025-10-31 15:36:08 +0700

    membuat button untuk dipilih user

--- app/src/main/java/com/example/questuserinput_163/TeksEdit.kt
+++ app/src/main/java/com/example/questuserinput_163/TeksEdit.kt
@@ -4,9 +4,11 @@ import androidx.compose.animation.expandHorizontally
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.selection.selectable
+import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
@@ -21,6 +23,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -83,7 +86,18 @@ fun FormatDataDiri(modifier: Modifier
             thickness = dimensionResource(id = R.dimen.padding_tipis),
             color = Color.DarkGray
         )
-
+        Button(
+            modifier = Modifier.fillMaxWidth(fraction = 1f),
+            // the button is enabled when the user makes a selection
+            enabled = textAlamat.isNotEmpty(),
+            onClick = {
+                nama=textNama
+                jenis=textJK
+                alamat=textAlamat
+            }
+        ) {
+            Text(text = stringResource(id = R.string.submit))
+        }
     }
 
 }
