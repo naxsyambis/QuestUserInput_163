d936ddbf378f38d24f477b0390d1bee42fe50a83
naxsyambis <naxsyambis@gmail.com>
2025-10-31 15:11:53 +0700

    membuat column yang akan berisi text nama

--- app/src/main/java/com/example/questuserinput_163/TeksEdit.kt
+++ app/src/main/java/com/example/questuserinput_163/TeksEdit.kt
@@ -1,11 +1,19 @@
 package com.example.questuserinput_163
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun FormatDataDiri(modifier: Modifier
@@ -21,5 +29,20 @@ fun FormatDataDiri(modifier: Modifier
     var jenis by remember { mutableStateOf(value = "") }
 
     val gender:List<String> = listOf("Laki-laki", "Perempuan")
+
+    Column(modifier = Modifier.padding(top = 50.dp)
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally){
+        OutlinedTextField(
+            value = textNama,
+            singleLine = true,
+            shape = MaterialTheme.shapes.large,
+            modifier = Modifier.width(width= 250.dp),
+            label = { Text(text = "Nama Lengkap") },
+            onValueChange = {
+                textNama = it
+            }
+        )
+    }
 
 }
