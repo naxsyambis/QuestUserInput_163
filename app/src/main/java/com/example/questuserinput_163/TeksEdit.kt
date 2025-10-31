3b541fca7070678358e7ace5d60e4dfb0b4412f6
naxsyambis <naxsyambis@gmail.com>
2025-10-31 15:25:33 +0700

    membuat text field alamat

--- app/src/main/java/com/example/questuserinput_163/TeksEdit.kt
+++ app/src/main/java/com/example/questuserinput_163/TeksEdit.kt
@@ -63,6 +63,16 @@ fun FormatDataDiri(modifier: Modifier
                 }
             }
         }
+        OutlinedTextField(
+            value = textAlamat,
+            singleLine = true,
+            modifier = Modifier.width(width = 250.dp),
+            label = {Text(text = "Alamat Lengkap")},
+            onValueChange = {
+                textAlamat = it
+            }
+        )
+
     }
 
 }
