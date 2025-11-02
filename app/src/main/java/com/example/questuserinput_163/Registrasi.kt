b0222b4cdc236a1d20be2b9a50a87782681fa004
naxsyambis <naxsyambis@gmail.com>
2025-11-02 14:32:13 +0700

    Membuat field tanggal lahir dengan memasukkan pengaturan tanggal

--- app/src/main/java/com/example/questuserinput_163/Registrasi.kt
+++ app/src/main/java/com/example/questuserinput_163/Registrasi.kt
@@ -2,32 +2,25 @@ package com.example.questuserinput_163
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.runtime.getValue
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
+import java.util.Calendar
+import android.app.DatePickerDialog
+import androidx.compose.foundation.clickable
+import androidx.compose.ui.res.stringResource
 
 @Composable
 fun FormRegistrasi(modifier: Modifier){
@@ -95,6 +88,38 @@ fun FormRegistrasi(modifier: Modifier){
             )
             Spacer(modifier = Modifier.height(12.dp))
 
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                val context = LocalContext.current
+                val calendar = Calendar.getInstance()
+                val year = calendar.get(Calendar.YEAR)
+                val month = calendar.get(Calendar.MONTH)
+                val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+                val datePickerDialog = DatePickerDialog( context,
+                    { _, selectedYear, selectedMonth, selectedDay ->
+                        tanggalLahir = "$selectedDay/${selectedMonth + 1}/$selectedYear"
+                    },
+                    year,
+                    month,
+                    day
+                )
+                OutlinedTextField(
+                    value = tanggalLahir,
+                    onValueChange = { },
+                    label = { Text("Tanggal Lahir") },
+                    shape = RoundedCornerShape (16.dp),
+                    modifier = Modifier
+                        .weight(1f)
+                        .clickable { datePickerDialog.show() },
+                    enabled = false,
+                    readOnly = true
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+            }
         }
     }
 }
