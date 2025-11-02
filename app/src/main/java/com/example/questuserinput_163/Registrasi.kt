e62362bb1628c8f21a11271c637469637ffdffae
naxsyambis <naxsyambis@gmail.com>
2025-11-02 14:09:13 +0700

    Membuat var dan val yang akan digunakan

--- app/src/main/java/com/example/questuserinput_163/Registrasi.kt
+++ app/src/main/java/com/example/questuserinput_163/Registrasi.kt
@@ -1,9 +1,22 @@
 package com.example.questuserinput_163
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 
 @Composable
 fun FormRegistrasi(modifier: Modifier){
+    var nama by remember { mutableStateOf("") }
+    var kota by remember { mutableStateOf("") }
+    var tanggalLahir by remember { mutableStateOf("") }
+    var rt by remember { mutableStateOf("") }
+    var rw by remember { mutableStateOf("") }
+    var umur by remember { mutableStateOf("") }
+    var gender by remember { mutableStateOf("") }
+    var isChecked by remember { mutableStateOf(false) }
 
+    val jenisKelamin = listOf("Laki-laki", "Perempuan")
 }
